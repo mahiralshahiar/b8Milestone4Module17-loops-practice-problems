dec0ceb6c7ceb9175c6775279312f7bba4b637e5
mahiralshahiar <mrmahiralshariar9@gmail.com>
2023-08-02 06:23:01 +0600

    practice numbers, for, even

---
+++ for.js
@@ -0,0 +1,32 @@
+// var roastGiven = 0;
+// while (roastGiven < 7) {
+//     console.log('roast den please')
+//     roastGiven++;
+// }
+
+// for (var roastGiven = 0; roastGiven < 7; roastGiven++) {
+//     console.log('roast den Please!!')
+// }
+
+// simple version 
+// console.log('simple for loop');
+
+// for (var i = 0; i < 7; i++) {
+//     console.log(i);
+// }
+
+
+// for (i = 0; i < 100; i++) {
+//     console.log(i);
+// }
+
+
+// for (var i = 0; i < 20; i += 2) {
+//     console.log(i);
+
+// }
+
+for (var i = 1; i < 20; i += 2) {
+    console.log(i);
+
+}
